7386478a4dee77637df10f1ef15c4b3f03b1492b
narumincho <16481886+narumincho@users.noreply.github.com>
2023-04-01 18:32:33 +0900

    サーバーに指定するように

--- src/main/java/com/buzzcre/buzzcre2/Buzzcre2.java
+++ src/main/java/com/buzzcre/buzzcre2/Buzzcre2.java
@@ -1,6 +1,8 @@
 package com.buzzcre.buzzcre2;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.text.Text;
 
 public class Buzzcre2 implements ModInitializer {
     /**
@@ -8,6 +10,9 @@ public class Buzzcre2 implements ModInitializer {
      */
     @Override
     public void onInitialize() {
-
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            handler.player.sendMessage(Text.of("バズクリワールドへようこそ！ なにかあったらチャットで発言してね！"));
+            System.out.println(handler.player.getName() + " が参加したようだ");
+        });
     }
 }
--- src/main/java/com/buzzcre/buzzcre2/client/Buzzcre2Client.java
+++ src/main/java/com/buzzcre/buzzcre2/client/Buzzcre2Client.java
@@ -1,7 +1,6 @@
 package com.buzzcre.buzzcre2.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 
 public class Buzzcre2Client implements ClientModInitializer {
     /**
@@ -9,8 +8,5 @@ public class Buzzcre2Client implements ClientModInitializer {
      */
     @Override
     public void onInitializeClient() {
-        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-            server.getCommandManager().executeWithPrefix(server.getCommandSource(), "/tell " + handler.player.getName() + " バズクリワールドへようこそ！ なにかあったらチャットで発言してね！");
-        });
     }
 }
